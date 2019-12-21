07aa028f140330af7398c5ddca9f422aff90a938
Nic de Sousa <15162654+nicdesousa@users.noreply.github.com>
2019-12-21 12:35:47 +0200

    Solution

--- solution/build.sh
+++ solution/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-./mvnw clean compile package -Dquarkus.profile=dev
+#./mvnw clean compile package -Dquarkus.profile=dev
+mvn clean compile package -Dquarkus.profile=dev
 docker build --rm -f src/main/docker/Dockerfile.amazoncorretto -t nicdesousa/mat-coding-challenge .
